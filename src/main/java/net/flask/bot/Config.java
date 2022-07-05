4262a31ad90d1e007a4c1df8fa7ad60504fe0f7b
Daniel Flask <78703851+notflask@users.noreply.github.com>
2022-07-05 22:31:28 +0200

    Убран TOKEN из Config :D

--- src/main/java/net/flask/bot/Config.java
+++ src/main/java/net/flask/bot/Config.java
@@ -1,7 +1,7 @@
 package net.flask.bot;
 
 public class Config {
-    public static final String TOKEN = "OTc3MTQ1NDQwNTM1NDQ1NTE1.Gqo6km.N5WPLIt5W5SUz-8WBzl40bIRkpdUyqQR-aWRL8";
+    public static final String TOKEN = "YOUR API TOKEN HERE";
     public static final String COMMAND_PREFIX = "!";
 
     public static final String EXAMPLE_COMMAND = COMMAND_PREFIX + "example";
